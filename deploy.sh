207a9f49f4e07bbfd535220c1e595396c76643c1
A-A-Z <dramatichamster@gmail.com>
2019-01-26 22:37:33 +1100

    Update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:a-a-z.github.io/colourlist.git master:gh-pages
+git push -f git@github.com:A-A-Z/colourlist.git master:gh-pages
 
 cd -
