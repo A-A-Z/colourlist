080b75aae013b59bd2edc121443a09f7a2111ed4
Paul K Lynch <dramatichamster@gmail.com>
2019-05-08 13:18:55 +1000

    List unit tests re-done

--- tests/unit/list.spec.js
+++ tests/unit/list.spec.js
@@ -2,8 +2,10 @@ import { shallowMount } from '@vue/test-utils'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import List from '@/components/List.vue'
-import { colourList } from '@/store/modules'
-import { SET_ACTIVE_COLOUR, SET_COLOURS } from '@/store/mutation-types.js'
+import { colourList, cloud } from '@/store/modules'
+import { SET_ACTIVE_COLOUR, SET_COLOURS, SET_SAVE_STATE } from '@/store/mutation-types.js'
+import { ON_NEW_COLOUR_INPUT } from '@/store/action-types'
+import { SAVE_STATES } from '@/constants'
 
 Vue.use(Vuex)
 
@@ -12,11 +14,16 @@ describe('List.vue', () => {
   let store
 
   beforeEach(() => {
-    store = new Vuex.Store({ modules: { colourList } })
-
-    wrapper = shallowMount(List, {
-      store
-    })
+    const { state, mutations, getters } = colourList
+    const colourListMock = { // Not needed, but still this is how it can be done
+      namespaced: true,
+      state,
+      mutations,
+      getters,
+      actions: { [ON_NEW_COLOUR_INPUT]: jest.fn() }
+    }
+    store = new Vuex.Store({ modules: { colourList: colourListMock, cloud } })
+    wrapper = shallowMount(List, { store })
   })
 
   it('renders List section', () => {
@@ -70,89 +77,63 @@ describe('List.vue', () => {
     expect(wrapper.findAll('.o-colour-list__item--active')).toHaveLength(1)
   })
 
-  // TODO Why no work!?
-  // it('having an active colour shows the delete icon for that colour', () => {
-  //   store.commit(`colourList/${SET_COLOURS}`, { list: ['#ff0000', '#00ff00', '#0000ff'] })
-  //   store.commit(`colourList/${SET_ACTIVE_COLOUR}`, '#00ff00')
-  //   expect(wrapper.findAll('.o-colour-list__delete')).toHaveLength(1)
-  // })
+  it('having an active colour shows the delete icon for that colour', () => {
+    store.commit(`colourList/${SET_COLOURS}`, { list: ['#ff0000', '#00ff00', '#0000ff'] })
+    store.commit(`colourList/${SET_ACTIVE_COLOUR}`, '#00ff00')
+    store.commit(`cloud/${SET_SAVE_STATE}`, SAVE_STATES.READY)
+    expect(wrapper.findAll('.o-colour-list__delete')).toHaveLength(1)
+  })
+
+  it('no delete option if disabled (loading)', () => {
+    store.commit(`colourList/${SET_COLOURS}`, { list: ['#ff0000', '#00ff00', '#0000ff'] })
+    store.commit(`colourList/${SET_ACTIVE_COLOUR}`, '#00ff00')
+    store.commit(`cloud/${SET_SAVE_STATE}`, SAVE_STATES.LOADING)
+    expect(wrapper.findAll('.o-colour-list__delete')).toHaveLength(0)
+  })
+
+  it('clicking on delete icon removes active colour', () => {
+    store.commit(`colourList/${SET_COLOURS}`, { list: ['#ff0000', '#00ff00', '#0000ff'] })
+    store.commit(`colourList/${SET_ACTIVE_COLOUR}`, '#00ff00')
+    store.commit(`cloud/${SET_SAVE_STATE}`, SAVE_STATES.READY)
+
+    wrapper
+      .find('.o-colour-list__delete')
+      .trigger('click')
+
+    expect(store.state.colourList.colours).toEqual(['#ff0000', '#0000ff'])
+  })
 
   it('renders correctly', () => {
     store.commit(`colourList/${SET_COLOURS}`, { list: ['#ff0000', '#00ff00', '#0000ff'] })
     store.commit(`colourList/${SET_ACTIVE_COLOUR}`, '#00ff00')
+    store.commit(`cloud/${SET_SAVE_STATE}`, SAVE_STATES.READY)
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  //
-  // it('clicking on item makes colour active', () => {
-  //   wrapper.setProps({
-  //     colours: [ '#ffffff' ],
-  //     setActiveColour: jest.fn()
-  //   })
-  //   wrapper
-  //     .find('.o-colour-list__item')
-  //     .trigger('click')
-  //
-  //   expect(wrapper.vm.setActiveColour).toHaveBeenCalledWith('#ffffff')
-  // })
-  //
-
-  //
-
-  //
-  // it('having an active colour shows the delete icon for that colour', () => {
-  //   wrapper.setProps({
-  //     colours: [
-  //       '#111111',
-  //       '#222222',
-  //       '#333333'
-  //     ],
-  //     activeColour: '#222222'
-  //   })
-  //   expect(wrapper.findAll('.o-colour-list__item--active .o-colour-list__delete')).toHaveLength(1)
-  // })
-  //
-  // it('clicking on delete icon calls deleteSavedColour function for that colour', () => {
-  //   wrapper.setProps({
-  //     colours: [
-  //       '#111111',
-  //       '#222222',
-  //       '#333333'
-  //     ],
-  //     activeColour: '#222222',
-  //     deleteSavedColour: jest.fn()
-  //   })
-  //   wrapper
-  //     .find('.o-colour-list__delete')
-  //     .trigger('click')
-  //
-  //   expect(wrapper.vm.deleteSavedColour).toHaveBeenCalledWith('#222222')
-  // })
-  //
-  // it('computed coloursSorted sorts colours by hue', () => {
-  //   wrapper.setProps({
-  //     colours: [
-  //       '#9999ff',
-  //       '#ff0000',
-  //       '#00ff00',
-  //       '#5555ff',
-  //       '#ff5555',
-  //       '#0000ff',
-  //       '#99ff99',
-  //       '#ff9999',
-  //       '#55ff55'
-  //     ]
-  //   })
-  //   expect(wrapper.vm.coloursSorted).toEqual([
-  //     '#ff9999',
-  //     '#ff5555',
-  //     '#ff0000',
-  //     '#99ff99',
-  //     '#55ff55',
-  //     '#00ff00',
-  //     '#9999ff',
-  //     '#5555ff',
-  //     '#0000ff'
-  //   ])
-  // })
+  it('computed coloursSorted sorts colours by hue', () => {
+    store.commit(`colourList/${SET_COLOURS}`, {
+      list: [
+        '#9999ff',
+        '#ff0000',
+        '#00ff00',
+        '#5555ff',
+        '#ff5555',
+        '#0000ff',
+        '#99ff99',
+        '#ff9999',
+        '#55ff55'
+      ]
+    })
+    expect(wrapper.vm.coloursSorted).toEqual([
+      '#ff9999',
+      '#ff5555',
+      '#ff0000',
+      '#99ff99',
+      '#55ff55',
+      '#00ff00',
+      '#9999ff',
+      '#5555ff',
+      '#0000ff'
+    ])
+  })
 })
